48f07d5f9c43767270965d0283acdc16014bf0cb
Paul <isakura313@gmail.com>
2021-03-25 12:56:03 +0300

    asadasd

--- js/code.js
+++ js/code.js
@@ -1,6 +1,10 @@
 import css from '../node_modules/bulma/css/bulma.min.css';
 import Element from './element';
-const root = document.querySelector("#root");
 Element('div', 'columns', '', '#root')
 Element('div', 'column has-text-centered', '', '.columns')
 Element('h1', 'title', 'Todo', '.column')
+Element('div', 'columns second_row', '', '#root');
+Element('div', 'column second_row_column', '', '.second_row')
+Element('div', 'giph', '', '.second_row_column')
+
+
