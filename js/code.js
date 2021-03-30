bcbdb0f6bcd41dd6c109fbfdcd5415cdf106b3c3
Paul <isakura313@gmail.com>
2021-03-30 13:04:05 +0300

    asad

--- js/code.js
+++ js/code.js
@@ -1,11 +1,41 @@
-import css from '../node_modules/bulma/css/bulma.min.css';
-import style from '../style.css'
-import Element from './element';
-Element('div', 'columns', '', '#root')
-Element('div', 'column has-text-centered', '', '.columns')
-Element('h1', 'title', 'Todo', '.column')
-Element('div', 'columns second_row', '', '#root');
-Element('div', 'column second_row_column', '', '.second_row')
-Element('div', 'giph', '', '.second_row_column')
+import { HotUpdateChunk } from "webpack";
+import css from "../node_modules/bulma/css/bulma.min.css";
+import style from "../style.css";
 
+const todos = ["Помыть кота", "купить i7"];
+
+import Element from "./element";
+Element("div", "columns", "", "#root", "beforeend");
+Element("div", "column has-text-centered", "", ".columns", "beforeend");
+Element("h1", "title", "Todo", ".column", "beforeend");
+Element("div", "columns second_row", "", "#root", "beforeend");
+// Element('div', 'column second_row_column', '', '.second_row')
+Element("div", "giph column is-one-third", "", ".second_row", "beforeend");
+Element(
+  "input",
+  "input is-primary column is-size-4",
+  "",
+  ".second_row",
+  "beforeend"
+);
+Element(
+  "button",
+  "button is-dark btn_plus is-size-4",
+  "+",
+  ".input",
+  "afterend"
+);
+
+// TODO из массива рисовались все дела
+// будем назначать дела последними детьми second_row
+Element("div", " columns third_row is-centered", "", "#root", "beforeend");
+Element("div", " column is-half todo_wrapper", "", ".third_row", "beforeend");
+
+todos.map((el) => {
+  Element("div", 
+  "todo is-size-3",
+   el, 
+   ".todo_wrapper", 
+   "beforeend");
+})
 
--- js/element.js
+++ js/element.js
@@ -1,8 +1,13 @@
-function Element(tag, cssClass, content = None, parentClass){
+function Element(tag, cssClass, content = None, parentElement, pos){
+    /*
+        tag - тип элемента в виде String
+        cssClass - класс в CSS в виде String
+        [content] - содержимое элемента
+        parentElement - родительский элемент
+    */
     let element = document.createElement(tag)
     element.className = cssClass;
     element.textContent = content
-    document.querySelector(`${parentClass}`).append(element);
+    document.querySelector(`${parentElement}`).insertAdjacentElement(pos, element)
 }
-
 export default Element
