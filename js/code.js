d0589837f8a0434e6649ef4a53ab1eabb4d9edc3
Your Name <you@example.com>
2021-04-01 13:20:14 +0300

    dfd

--- js/code.js
+++ js/code.js
@@ -1,8 +1,9 @@
-import { HotUpdateChunk } from "webpack";
 import css from "../node_modules/bulma/css/bulma.min.css";
 import style from "../style.css";
 
-const todos = ["Помыть кота", "купить i7"];
+// const todos = ["Помыть кота", "купить i7", "новое дело"];
+let todos = Object.values(localStorage);
+
 
 import Element from "./element";
 Element("div", "columns", "", "#root", "beforeend");
@@ -12,18 +13,18 @@ Element("div", "columns second_row", "", "#root", "beforeend");
 // Element('div', 'column second_row_column', '', '.second_row')
 Element("div", "giph column is-one-third", "", ".second_row", "beforeend");
 Element(
-  "input",
-  "input is-primary column is-size-4",
-  "",
-  ".second_row",
-  "beforeend"
+    "input",
+    "input is-primary column is-size-4",
+    "",
+    ".second_row",
+    "beforeend"
 );
 Element(
-  "button",
-  "button is-dark btn_plus is-size-4",
-  "+",
-  ".input",
-  "afterend"
+    "button",
+    "button is-dark btn_plus is-size-4",
+    "+",
+    ".input",
+    "afterend"
 );
 
 // TODO из массива рисовались все дела
@@ -31,11 +32,41 @@ Element(
 Element("div", " columns third_row is-centered", "", "#root", "beforeend");
 Element("div", " column is-half todo_wrapper", "", ".third_row", "beforeend");
 
-todos.map((el) => {
-  Element("div", 
-  "todo is-size-3",
-   el, 
-   ".todo_wrapper", 
-   "beforeend");
-})
+drawDeals()
 
+function drawDeals() {
+    document.querySelector('.todo_wrapper').innerHTML = ""
+    todos = Object.values(localStorage)
+    todos.map((el) => {
+        Element("div",
+            "todo is-size-3",
+            `${el} <button class="button is-danger">
+                    del </button>`,
+            ".todo_wrapper",
+            "beforeend");
+    })
+    deleteItem()
+}
+
+function deleteItem() {
+    let btns_delete = document.querySelectorAll('.is-danger')
+    let arr_delete = Array.from(btns_delete);
+    arr_delete.map((el, index) => {
+        el.onclick = function() {
+            this.parentNode.remove()
+                // todos.splice(index, 1)
+            localStorage.removeItem(index)
+        }
+    })
+}
+
+function addDeal() {
+    let data = document.querySelector('input');
+    // todos.push(data.value)
+    localStorage.setItem(Object.values(localStorage).length, data.value)
+    console.log(todos)
+    data.value = ''
+    drawDeals()
+}
+
+document.querySelector(".btn_plus").onclick = addDeal;
--- js/element.js
+++ js/element.js
@@ -1,4 +1,4 @@
-function Element(tag, cssClass, content = None, parentElement, pos){
+function Element(tag, cssClass, content = None, parentElement, pos) {
     /*
         tag - тип элемента в виде String
         cssClass - класс в CSS в виде String
@@ -7,7 +7,7 @@ function Element(tag, cssClass, content = None, parentElement, pos){
     */
     let element = document.createElement(tag)
     element.className = cssClass;
-    element.textContent = content
+    element.innerHTML = content
     document.querySelector(`${parentElement}`).insertAdjacentElement(pos, element)
 }
 export default Element
